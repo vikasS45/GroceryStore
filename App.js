40f1885010d495ea257d005eea4f691c2d0ceb6b
vikasS45 <vsalunke758@gmail.com>
2020-12-28 12:45:41 +0530

    Changes

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>OM SAI RAM</Text>
+      <Text>OM SAI RAM !!</Text>
       <StatusBar style="auto" />
     </View>
   );
